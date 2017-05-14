ddc48f0a3f8781924b35ea903f65faadba9e20e2
Hsuan Lee <boczeratul@gmail.com>
2017-05-14 15:20:02 +0800

    log data

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.get('/webhook', (req, res) => {
 
 app.post('/webhook', (req, res) => {
   const data = req.body;
-  webClient.chat.postMessage('@hsuan', `Message data: ${JSON.stringify(req)}`);
+  webClient.chat.postMessage('@hsuan', `Message data: ${data}`);
 
   // Make sure this is a page subscription
   if (data.object === 'page') {
