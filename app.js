f859d82c4232f2b66f607c428d6a5c91c227700f
Hsuan Lee <boczeratul@gmail.com>
2017-05-14 12:58:38 +0800

    switch to express app

--- app.js
+++ app.js
@@ -1,45 +1,18 @@
-import { RtmClient, WebClient, RTM_EVENTS } from '@slack/client';
-import EnvConfig from './env.config.json';
-import { parseCommand, Constants } from './lib';
-import { executeCommand, helpOptions } from './command';
+import express from 'express';
 
-const rtmClient = new RtmClient(EnvConfig.Slack.BOT_TOKEN);
-const webClient = new WebClient(EnvConfig.Slack.BOT_TOKEN);
+const app = express();
 
-rtmClient.on(RTM_EVENTS.MESSAGE, (message) => {
-  if (!/^D/.test(message.channel) || !message.text) {
-    // has to be direct message to bot
-    return;
-  }
-
-  const user = rtmClient.dataStore.getUserById(message.user);
-  if (!user || user.is_bot) {
-    // should not process my own message
-    return;
-  }
-
-  parseCommand(message.text)
-    .then(executeCommand)
-    .then(({ response, options }) => webClient.chat.postMessage(message.channel, response, options))
-    .catch((error) => {
-      switch (error.message || error) {
-        case Constants.INVALID_COMMAND:
-        case Constants.INVALID_INPUT:
-          return webClient.chat.postMessage(message.channel, '', helpOptions);
-
-        case Constants.NO_SUCH_TRAINEE:
-          return webClient.chat.postMessage(message.channel, '', {
-            attachments: [{
-              color: 'danger',
-              text: '找不到這個學員',
-            }],
-          });
-
-        case Constants.COMMAND_FAILED:
-        default:
-          return webClient.chat.postMessage(message.channel, error.stack);
-      }
-    });
+app.get('/', (req, res) => {
+  res.status(200).send('歡迎來到漢堡王！').end();
 });
 
-rtmClient.start();
+app.get('/webhook', (req, res) => {
+  res.status(200).send('歡迎來到漢堡王！').end();
+});
+
+// Start the server
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+  console.log('Press Ctrl+C to quit.');
+});
