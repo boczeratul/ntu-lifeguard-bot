9d0a37640808ebdace18ce5cbbd799fd13b7be3b
Hsuan Lee <boczeratul@gmail.com>
2017-05-14 13:18:02 +0800

    add stub

--- app.js
+++ app.js
@@ -10,9 +10,38 @@ app.get('/', (req, res) => {
   res.status(200).send('歡迎來到漢堡王！').end();
 });
 
-app.get('/webhook', (req, res) => {
-  res.status(200).send('歡迎來到漢堡王！').end();
-  webClient.chat.postMessage('@hsuan', JSON.stringify(req.body));
+const receivedMessage = (event) => {
+  // Putting a stub for now, we'll expand it in the following steps
+  webClient.chat.postMessage('@hsuan', `Message data: ${event.message}`);
+};
+
+app.post('/webhook', (req, res) => {
+  const data = req.body;
+
+  // Make sure this is a page subscription
+  if (data.object === 'page') {
+    // Iterate over each entry - there may be multiple if batched
+    data.entry.forEach((entry) => {
+      const pageID = entry.id;
+      const timeOfEvent = entry.time;
+
+      // Iterate over each messaging event
+      entry.messaging.forEach((event) => {
+        if (event.message) {
+          receivedMessage(event);
+        } else {
+          console.log(`Webhook received unknown event: ${event}`);
+        }
+      });
+    });
+
+    // Assume all went well.
+    //
+    // You must send back a 200, within 20 seconds, to let us know
+    // you've successfully received the callback. Otherwise, the request
+    // will time out and we will keep trying to resend.
+    res.sendStatus(200);
+  }
 });
 
 // Start the server
